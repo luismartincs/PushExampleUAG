dad9c13e72bccdb702e6640e3fb98df64dd1a7d6
Luis de Jesus Martin Castillo <luismartin.925@gmail.com>
2016-07-08 18:22:36 -0500

    Initial Commit

---
+++ PushExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PushExample
+//
+//  Created by Luis de Jesus Martin Castillo on 08/07/16.
+//  Copyright © 2016 Luis de Jesus Martin Castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
